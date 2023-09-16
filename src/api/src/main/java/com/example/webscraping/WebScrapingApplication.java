325d8a8ca5e0bdda83993bfee390dfbcad4adf87
Andrei Sarateanu <sarateanuandrei28@gmail.com>
2023-09-16 17:43:26 +0300

    main-database-connection: database connected & test Real User Agent

--- src/api/src/main/java/com/example/webscraping/WebScrapingApplication.java
+++ src/api/src/main/java/com/example/webscraping/WebScrapingApplication.java
@@ -9,5 +9,4 @@ public class WebScrapingApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebScrapingApplication.class, args);
     }
-
 }
--- src/api/src/main/java/com/example/webscraping/service/impl/ScraperServiceImpl.java
+++ src/api/src/main/java/com/example/webscraping/service/impl/ScraperServiceImpl.java
@@ -2,15 +2,18 @@ package com.example.webscraping.service.impl;
 
 import com.example.webscraping.model.ResponseDTO;
 import com.example.webscraping.service.ScraperService;
-import liquibase.repackaged.org.apache.commons.lang3.StringUtils;
+import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -18,136 +21,103 @@ import java.util.Set;
 
 
 @Service
+@RequiredArgsConstructor
 public class ScraperServiceImpl implements ScraperService {
     //Reading data from property file to a list
     @Value("#{'${website.urls}'.split(',')}")
     List<String> urls;
 
+
+    public void test(Set<ResponseDTO> responseDTOS, String url) throws IOException {
+        String apiKey = "b84fea938e623916c18c199a04817b5c";
+        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
+        Document document = Optional.of(Jsoup.connect(scrapedUrl).get()).orElseThrow();
+        Element element = Optional.ofNullable(document.getElementById("content")).orElseThrow(IOException::new);
+        Elements elements = Optional.of(element.getElementsByTag("a")).orElseThrow();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) document.connection();
+        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+        httpURLConnection.setRequestProperty("X-MyHeader", "123");
+        httpURLConnection.setRequestMethod("GET");
+        if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            for (Element ads : elements) {
+                ResponseDTO responseDTO = new ResponseDTO();
+                if (StringUtils.hasText(ads.attr("title"))) {
+                    responseDTO.setTitle(ads.attr("title"));
+                    responseDTO.setUrl(ads.attr("href"));
+                }
+                if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
+            }
+        }
+
+    }
+
+
     @Override
-    public Set<ResponseDTO> getVehicleByModel(String vehicleModel) {
+    public Set<ResponseDTO> getVehicleByModel(String vehicleModel) throws IOException {
         //Using a set here to only store unique elements
         Set<ResponseDTO> responseDTOS = new HashSet<>();
         //Traversing through the urls
         for (String url : urls) {
 
             if (url.contains("ikman")) {
-                //method to extract data from Ikman.lk
                 getVehicleByModelFromIkman(responseDTOS, url + vehicleModel);
+            } else if (url.contains("riyasewana")) {
+                getVehicleByModelFromRiyase(responseDTOS, url + vehicleModel);
             }
-//            else if (url.contains("riyasewana")) {
-//                //method to extract Data from riyasewana.com
-//                getVehicleByModelFromRiyase(responseDTOS, url + vehicleModel);
-//            }
         }
 
         return responseDTOS;
     }
 
     @Override
-    public void getVehicleByModelFromRiyase(Set<ResponseDTO> responseDTOS, String url) {
-        try {
-            String apiKey = "b84fea938e623916c18c199a04817b5c";
-            String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
-            Document document = Jsoup.connect(scrapedUrl).get();
-            //Selecting the element which contains the ad list
-            Element element = Optional.ofNullable(document.getElementById("content")).orElseThrow();
-            //getting all the <a> tag elements inside the content div tag
-            Elements elements = Optional.of(element.getElementsByTag("a")).orElseThrow();
-            //traversing through the elements
-            for (Element ads : elements) {
-                ResponseDTO responseDTO = new ResponseDTO();
+    public void getVehicleByModelFromRiyase(Set<ResponseDTO> responseDTOS, String url) throws IOException {
+        String apiKey = "b84fea938e623916c18c199a04817b5c";
+        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
+        Document document = Optional.of(Jsoup.connect(scrapedUrl).get()).orElseThrow();
+        //Selecting the element which contains the ad list
+        Element element = Optional.ofNullable(document.getElementById("content")).orElseThrow(IOException::new);
+        //getting all the <a> tag elements inside the content div tag
+        Elements elements = Optional.of(element.getElementsByTag("a")).orElseThrow();
+        //traversing through the elements
+        for (Element ads : elements) {
+            ResponseDTO responseDTO = new ResponseDTO();
 
-                if (!StringUtils.isEmpty(ads.attr("title"))) {
-                    //mapping data to the model class
-                    responseDTO.setTitle(ads.attr("title"));
-                    responseDTO.setUrl(ads.attr("href"));
-                }
-                if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
+            if (StringUtils.hasText(ads.attr("title"))) {
+                //mapping data to the model class
+                responseDTO.setTitle(ads.attr("title"));
+                responseDTO.setUrl(ads.attr("href"));
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
+
         }
-
-
     }
 
     @Override
-    public void getVehicleByModelFromIkman(Set<ResponseDTO> responseDTOS, String url) {
-        try {
-            String apiKey = "b84fea938e623916c18c199a04817b5c";
-            String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
-            Document document = Jsoup.connect(scrapedUrl).get();
-            Element element = Optional.ofNullable(document.getElementsByClass("list--3NxGO").first()).orElseThrow();
-            //getting all the <a> tag elements inside the list-       -3NxGO class
-            Elements elements = element.getElementsByTag("a");
+    public void getVehicleByModelFromIkman(Set<ResponseDTO> responseDTOS, String url) throws IOException {
+        String apiKey = "b84fea938e623916c18c199a04817b5c";
+        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
+        Document document = Optional.of(Jsoup.connect(scrapedUrl).get()).orElseThrow(IOException::new);
+        Element element = Optional.ofNullable(document.getElementsByClass("list--3NxGO").first()).orElseThrow();
+        //getting all the <a> tag elements inside the list-       -3NxGO class
+        Elements elements = element.getElementsByTag("a");
 
-            for (Element ads : elements) {
+        for (Element ads : elements) {
 
-                ResponseDTO responseDTO = new ResponseDTO();
+            ResponseDTO responseDTO = new ResponseDTO();
 
-                if (StringUtils.isNotEmpty(ads.attr("href"))) {
-                    //mapping data to our model class
-                    responseDTO.setTitle(ads.attr("title"));
-                    responseDTO.setUrl("https://ikman.lk" + ads.attr("href"));
-                }
-                if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
-
+            if (StringUtils.hasText(ads.attr("href"))) {
+                //mapping data to our model class
+                responseDTO.setTitle(ads.attr("title"));
+                responseDTO.setUrl("https://ikman.lk" + ads.attr("href"));
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
+
         }
+
     }
 
-
-    private void extractDataFromRiyasewana(Set<ResponseDTO> responseDTOS, String url) {
-
-        try {
-            //loading the HTML to a Document Object
-            Document document = Jsoup.connect(url).get();
-            //Selecting the element which contains the ad list
-            Element element = Optional.ofNullable(document.getElementById("content")).orElseThrow();
-            //getting all the <a> tag elements inside the content div tag
-            Elements elements = Optional.of(element.getElementsByTag("a")).orElseThrow();
-            //traversing through the elements
-            for (Element ads : elements) {
-                ResponseDTO responseDTO = new ResponseDTO();
-
-                if (!StringUtils.isEmpty(ads.attr("title"))) {
-                    //mapping data to the model class
-                    responseDTO.setTitle(ads.attr("title"));
-                    responseDTO.setUrl(ads.attr("href"));
-                }
-                if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private void extractDataFromIkman(Set<ResponseDTO> responseDTOS, String url) {
-        try {
-            //loading the HTML to a Document Object
-            Document document = Jsoup.connect(url).get();
-//Selecting the element which contains the ad list
-            Element element = Optional.ofNullable(document.getElementsByClass("list--3NxGO").first()).orElseThrow();
-            //getting all the <a> tag elements inside the list-       -3NxGO class
-            Elements elements = element.getElementsByTag("a");
-
-            for (Element ads : elements) {
-
-                ResponseDTO responseDTO = new ResponseDTO();
-
-                if (StringUtils.isNotEmpty(ads.attr("href"))) {
-                    //mapping data to our model class
-                    responseDTO.setTitle(ads.attr("title"));
-                    responseDTO.setUrl("https://ikman.lk" + ads.attr("href"));
-                }
-                if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
-
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
 
 }
+
+
+
