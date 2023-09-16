21fdfaf324cbb6f267800e0dc15a39892bc80201
Andrei Sarateanu <sarateanuandrei28@gmail.com>
2023-09-16 17:43:26 +0300

    main-database-connection: database connected & test Real User Agent

--- src/api/src/main/java/com/example/webscraping/controller/ScraperController.java
+++ src/api/src/main/java/com/example/webscraping/controller/ScraperController.java
@@ -7,18 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.Set;
 
 @RestController
 @RequestMapping(path = "/")
 @RequiredArgsConstructor
 public class ScraperController {
-
     private final ScraperService scraperService;
 
     @GetMapping(path = "/{vehicleModel}")
-    public Set<ResponseDTO> getVehicleByModel(@PathVariable String vehicleModel) {
+    public Set<ResponseDTO> getVehicleByModel(@PathVariable String vehicleModel) throws IOException {
         return scraperService.getVehicleByModel(vehicleModel);
     }
 
--- src/api/src/main/java/com/example/webscraping/service/ScraperService.java
+++ src/api/src/main/java/com/example/webscraping/service/ScraperService.java
@@ -2,10 +2,11 @@ package com.example.webscraping.service;
 
 import com.example.webscraping.model.ResponseDTO;
 
+import java.io.IOException;
 import java.util.Set;
 
 public interface ScraperService {
-    Set<ResponseDTO> getVehicleByModel(String vehicleModel);
-    void getVehicleByModelFromRiyase(Set<ResponseDTO> responseDTOS, String url);
-    void getVehicleByModelFromIkman(Set<ResponseDTO> responseDTOS, String url);
+    Set<ResponseDTO> getVehicleByModel(String vehicleModel) throws IOException;
+    void getVehicleByModelFromRiyase(Set<ResponseDTO> responseDTOS, String url) throws IOException;
+    void getVehicleByModelFromIkman(Set<ResponseDTO> responseDTOS, String url) throws IOException;
 }
