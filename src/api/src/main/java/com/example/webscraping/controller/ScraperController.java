d75a8c557e8c17ce67a1f3b38a90e25721d9d5bf
Andrei Sarateanu <sarateanuandrei28@gmail.com>
2023-09-18 14:51:10 +0300

    main-database-connection: anti scrap header addet

--- src/api/src/main/java/com/example/webscraping/controller/ScraperController.java
+++ src/api/src/main/java/com/example/webscraping/controller/ScraperController.java
@@ -2,11 +2,9 @@ package com.example.webscraping.controller;
 
 import com.example.webscraping.model.ResponseDTO;
 import com.example.webscraping.service.ScraperService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
--- src/api/src/main/java/com/example/webscraping/service/impl/ScraperServiceImpl.java
+++ src/api/src/main/java/com/example/webscraping/service/impl/ScraperServiceImpl.java
@@ -10,14 +10,12 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.net.URL;
+import java.util.*;
 
 
 @Service
@@ -28,16 +26,37 @@ public class ScraperServiceImpl implements ScraperService {
     List<String> urls;
 
 
-    public void test(Set<ResponseDTO> responseDTOS, String url) throws IOException {
+    @Override
+    public Set<ResponseDTO> getVehicleByModel(String vehicleModel) throws IOException {
+        Set<ResponseDTO> responseDTOS = new HashSet<>();
+        for (String url : urls) {
+
+            if (url.contains("ikman")) {
+                getVehicleByModelFromIkman(responseDTOS, url + vehicleModel, Map.of("X-POWERED-BY", "Spring Framework 6"));
+            } else if (url.contains("riyasewana")) {
+                getVehicleByModelFromRiyase(responseDTOS, url + vehicleModel, Map.of("X-POWERED-BY", "Spring Framework 6"));
+            }
+        }
+
+        return responseDTOS;
+    }
+
+    @Override
+    public void getVehicleByModelFromRiyase(Set<ResponseDTO> responseDTOS, String url, @RequestHeader Map<String, String> header) throws IOException {
         String apiKey = "b84fea938e623916c18c199a04817b5c";
-        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
+        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url + "&keep_headers=true&country_code=us";
+        URL url1 = new URL(scrapedUrl);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url1.openConnection();
+        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+        httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0"); //make rotation
+        httpURLConnection.setRequestProperty("X-MyHeader", "123");
+        httpURLConnection.setRequestProperty("Referer", "https://www.google.com/");// referer
+        httpURLConnection.setRequestMethod("GET");
         Document document = Optional.of(Jsoup.connect(scrapedUrl).get()).orElseThrow();
         Element element = Optional.ofNullable(document.getElementById("content")).orElseThrow(IOException::new);
         Elements elements = Optional.of(element.getElementsByTag("a")).orElseThrow();
-        HttpURLConnection httpURLConnection = (HttpURLConnection) document.connection();
-        httpURLConnection.setRequestProperty("Content-Type", "application/json");
-        httpURLConnection.setRequestProperty("X-MyHeader", "123");
-        httpURLConnection.setRequestMethod("GET");
+
+
         if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
             for (Element ads : elements) {
                 ResponseDTO responseDTO = new ResponseDTO();
@@ -48,54 +67,19 @@ public class ScraperServiceImpl implements ScraperService {
                 if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
             }
         }
-
-    }
-
-
-    @Override
-    public Set<ResponseDTO> getVehicleByModel(String vehicleModel) throws IOException {
-        //Using a set here to only store unique elements
-        Set<ResponseDTO> responseDTOS = new HashSet<>();
-        //Traversing through the urls
-        for (String url : urls) {
-
-            if (url.contains("ikman")) {
-                getVehicleByModelFromIkman(responseDTOS, url + vehicleModel);
-            } else if (url.contains("riyasewana")) {
-                getVehicleByModelFromRiyase(responseDTOS, url + vehicleModel);
-            }
-        }
-
-        return responseDTOS;
     }
 
     @Override
-    public void getVehicleByModelFromRiyase(Set<ResponseDTO> responseDTOS, String url) throws IOException {
+    public void getVehicleByModelFromIkman(Set<ResponseDTO> responseDTOS, String url, @RequestHeader Map<String, String> header) throws IOException {
         String apiKey = "b84fea938e623916c18c199a04817b5c";
-        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
-        Document document = Optional.of(Jsoup.connect(scrapedUrl).get()).orElseThrow();
-        //Selecting the element which contains the ad list
-        Element element = Optional.ofNullable(document.getElementById("content")).orElseThrow(IOException::new);
-        //getting all the <a> tag elements inside the content div tag
-        Elements elements = Optional.of(element.getElementsByTag("a")).orElseThrow();
-        //traversing through the elements
-        for (Element ads : elements) {
-            ResponseDTO responseDTO = new ResponseDTO();
-
-            if (StringUtils.hasText(ads.attr("title"))) {
-                //mapping data to the model class
-                responseDTO.setTitle(ads.attr("title"));
-                responseDTO.setUrl(ads.attr("href"));
-            }
-            if (responseDTO.getUrl() != null) responseDTOS.add(responseDTO);
-
-        }
-    }
-
-    @Override
-    public void getVehicleByModelFromIkman(Set<ResponseDTO> responseDTOS, String url) throws IOException {
-        String apiKey = "b84fea938e623916c18c199a04817b5c";
-        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url;
+        String scrapedUrl = "http://api.scraperapi.com?api_key=" + apiKey + "&url=" + url + "&keep_headers=true&country_code=us";
+        URL url1 = new URL(scrapedUrl);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url1.openConnection();
+        httpURLConnection.setRequestProperty("Content-Type", "application/json");
+        httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0"); //make rotation
+        httpURLConnection.setRequestProperty("X-MyHeader", "123");
+        httpURLConnection.setRequestProperty("Referer", "https://www.google.com/");// referer
+        httpURLConnection.setRequestMethod("GET");
         Document document = Optional.of(Jsoup.connect(scrapedUrl).get()).orElseThrow(IOException::new);
         Element element = Optional.ofNullable(document.getElementsByClass("list--3NxGO").first()).orElseThrow();
         //getting all the <a> tag elements inside the list-       -3NxGO class
